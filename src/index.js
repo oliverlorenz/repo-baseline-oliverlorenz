67a2334d65d6571047000c6f091937a6de78140c
Oliver Lorenz <mail@oliverlorenz.com>
2018-11-04 17:14:45 +0100

    save

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-const packageJson = require('../package.json')
+const packageJson = require('../package.json');
+const fileAvailable = require('repo-baseline-file-available');
+
 function getRules() {
     return packageJson['repo-baseline']
 }
 
+function validate(options, callback) {
+    return fileAvailable(fileAvailable, options)
+}
+
 module.exports = {
-    getRules
+    getRules,
+    validate
 }
