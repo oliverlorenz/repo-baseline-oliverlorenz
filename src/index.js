d833d56aba272e237e913967bc13bdbfeec6c8d0
Oliver Lorenz <mail@oliverlorenz.com>
2018-11-06 21:43:42 +0100

    save

--- src/index.js
+++ src/index.js
@@ -1,36 +1,6 @@
 const packageJson = require('../package.json');
 const RuleSet = require('repo-baseline-ruleset');
 
-/*
-const fileAvailable = require('repo-baseline-file-available');
-
-function getRules() {
-    return packageJson['repo-baseline'];
-}
-
-function getRulesOfSubmodules() {
-    const rules = [];
-    packageJson['repo-baseline'].forEach((ruleset) => {
-        rules = rules.concat(require(ruleset.name).getRules())
-    })
-    return rules;
-}
-
-function run(repoPath, options, level, callback) {
-    console.log(packageJson.name)
-    return Promise.resolve(getRules())
-        .each((ruleSet) => {
-            const package = require(ruleSet.name);
-            return package.run(repoPath, ruleSet.options, callback)
-        });
-}
-
-module.exports = {
-    getRules,
-    run
-}
-*/
-
 module.exports = (pluginManager, repoPath) => {
     return RuleSet(pluginManager, repoPath, packageJson['repo-baseline']);
 }
